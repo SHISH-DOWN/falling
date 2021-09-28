f096a53ceadb15124f649b58fced637fb9f2a68b
Mikhail Zhukevich <zhukevich_ma_19@student.grsu.by>
2021-09-28 06:59:17 +0300

    Добавьте файлы проекта.

---
+++ falling/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿
+namespace falling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.textBoxV = new System.Windows.Forms.TextBox();
+            this.textBoxAngle = new System.Windows.Forms.TextBox();
+            this.textBoxMass = new System.Windows.Forms.TextBox();
+            this.textBoxResistanceMultiplier = new System.Windows.Forms.TextBox();
+            this.textBoxResistPow = new System.Windows.Forms.TextBox();
+            this.textBoxDelta = new System.Windows.Forms.TextBox();
+            this.chartxt = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonParse = new System.Windows.Forms.Button();
+            this.buttonPreStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chartxt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(3, 33);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX.TabIndex = 0;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(3, 77);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 20);
+            this.textBoxY.TabIndex = 1;
+            // 
+            // textBoxV
+            // 
+            this.textBoxV.Location = new System.Drawing.Point(3, 123);
+            this.textBoxV.Name = "textBoxV";
+            this.textBoxV.Size = new System.Drawing.Size(100, 20);
+            this.textBoxV.TabIndex = 2;
+            // 
+            // textBoxAngle
+            // 
+            this.textBoxAngle.Location = new System.Drawing.Point(3, 163);
+            this.textBoxAngle.Name = "textBoxAngle";
+            this.textBoxAngle.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAngle.TabIndex = 3;
+            // 
+            // textBoxMass
+            // 
+            this.textBoxMass.Location = new System.Drawing.Point(3, 205);
+            this.textBoxMass.Name = "textBoxMass";
+            this.textBoxMass.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMass.TabIndex = 4;
+            // 
+            // textBoxResistanceMultiplier
+            // 
+            this.textBoxResistanceMultiplier.Location = new System.Drawing.Point(3, 245);
+            this.textBoxResistanceMultiplier.Name = "textBoxResistanceMultiplier";
+            this.textBoxResistanceMultiplier.Size = new System.Drawing.Size(100, 20);
+            this.textBoxResistanceMultiplier.TabIndex = 5;
+            // 
+            // textBoxResistPow
+            // 
+            this.textBoxResistPow.Location = new System.Drawing.Point(3, 283);
+            this.textBoxResistPow.Name = "textBoxResistPow";
+            this.textBoxResistPow.Size = new System.Drawing.Size(100, 20);
+            this.textBoxResistPow.TabIndex = 6;
+            // 
+            // textBoxDelta
+            // 
+            this.textBoxDelta.Location = new System.Drawing.Point(3, 319);
+            this.textBoxDelta.Name = "textBoxDelta";
+            this.textBoxDelta.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDelta.TabIndex = 7;
+            // 
+            // chartxt
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartxt.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartxt.Legends.Add(legend1);
+            this.chartxt.Location = new System.Drawing.Point(12, 345);
+            this.chartxt.Name = "chartxt";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartxt.Series.Add(series1);
+            this.chartxt.Size = new System.Drawing.Size(300, 95);
+            this.chartxt.TabIndex = 8;
+            this.chartxt.Text = "chart1";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(339, 345);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(300, 95);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chartyt";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(233, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 256);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "v";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(109, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "angle";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(109, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "mass";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(109, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "R";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(109, 290);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "n";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(109, 329);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "dt";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Enabled = false;
+            this.buttonStart.Location = new System.Drawing.Point(687, 316);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 19;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(687, 345);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 20;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            // 
+            // buttonParse
+            // 
+            this.buttonParse.Location = new System.Drawing.Point(196, 316);
+            this.buttonParse.Name = "buttonParse";
+            this.buttonParse.Size = new System.Drawing.Size(75, 23);
+            this.buttonParse.TabIndex = 21;
+            this.buttonParse.Text = "Parse";
+            this.buttonParse.UseVisualStyleBackColor = true;
+            this.buttonParse.Click += new System.EventHandler(this.buttonParse_Click);
+            // 
+            // buttonPreStart
+            // 
+            this.buttonPreStart.Enabled = false;
+            this.buttonPreStart.Location = new System.Drawing.Point(606, 316);
+            this.buttonPreStart.Name = "buttonPreStart";
+            this.buttonPreStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonPreStart.TabIndex = 22;
+            this.buttonPreStart.Text = "PreStart";
+            this.buttonPreStart.UseVisualStyleBackColor = true;
+            this.buttonPreStart.Click += new System.EventHandler(this.buttonPreStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonPreStart);
+            this.Controls.Add(this.buttonParse);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.chartxt);
+            this.Controls.Add(this.textBoxDelta);
+            this.Controls.Add(this.textBoxResistPow);
+            this.Controls.Add(this.textBoxResistanceMultiplier);
+            this.Controls.Add(this.textBoxMass);
+            this.Controls.Add(this.textBoxAngle);
+            this.Controls.Add(this.textBoxV);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chartxt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.TextBox textBoxV;
+        private System.Windows.Forms.TextBox textBoxAngle;
+        private System.Windows.Forms.TextBox textBoxMass;
+        private System.Windows.Forms.TextBox textBoxResistanceMultiplier;
+        private System.Windows.Forms.TextBox textBoxResistPow;
+        private System.Windows.Forms.TextBox textBoxDelta;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartxt;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonParse;
+        private System.Windows.Forms.Button buttonPreStart;
+    }
+}
+
---
+++ falling/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Math;
+
+
+namespace falling
+{
+    
+    public partial class Form1 : Form
+    {
+        public double x,y,xprev,yprev,x0,y0,v,v0,u,vx,vy,vxprev,vyprev,vx0,vy0,R,dt,n,Fx,Fy,m,dt2,xmax,ymax,tmax,angle,ax,ay,t,t1;
+        public const double G = 9.81;
+        private void buttonPreStart_Click(object sender, EventArgs e)
+        {
+            xprev = x0;
+            yprev = y0;
+            v = v0;
+            t = 0;
+            vxprev = vx0;
+            vyprev = vy0;
+            xmax=Double.MinValue;
+            ymax=Double.MinValue;
+            dt2=5*dt;
+
+            while (xprev > 0)
+            {
+                Fx = -R * Pow(v, n - 1) * vx;
+                Fy= -1*R*Pow(v,n - 1)*vy - m*G;
+                ax = Fx / m;
+                ay = Fy / m;
+                vx = vxprev + ax * dt2;
+                vy = vyprev + ay * dt2;
+                x = xprev + vx * dt2 + ax * Pow(dt2, 2) / 2;
+                y = yprev + vy * dt2 + ay * Pow(dt2, 2) / 2;
+                if (x >= xmax)
+                {
+                    xmax = x;
+                }
+
+                if (y >= ymax)
+                {
+                    ymax = y;
+                }
+
+                xprev = x;
+                yprev = y;
+                t = t + dt2;
+                vxprev = vx;
+                vyprev = vy;
+
+
+            }
+
+            MessageBox.Show($"xmax={xmax} ymax={ymax}");
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = true;
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonParse_Click(object sender, EventArgs e)
+        {
+            Double.TryParse(textBoxX.Text, out x0);
+            Double.TryParse(textBoxY.Text, out y0);
+            Double.TryParse(textBoxV.Text, out v0);
+            Double.TryParse(textBoxAngle.Text, out angle);
+            Double.TryParse(textBoxMass.Text, out m);
+            Double.TryParse(textBoxResistanceMultiplier.Text, out R);
+            Double.TryParse(textBoxResistPow.Text, out n);
+            Double.TryParse(textBoxDelta.Text, out dt);
+            vx0 = v0 * Cos(angle*PI/180);
+            vy0 = v0 * Sin(angle*PI/180);
+            buttonPreStart.Enabled = true;
+            
+
+        }
+    }
+}
